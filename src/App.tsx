de3ae1e2535532f8597a725f68a84a9997c3aa3e
MrBiedny <MrBiedny@gmail.com>
2025-02-15 14:34:12 +0100

    add spinner component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
+
 import Search from "./components/Search";
+import Spinner from "./components/Spinner";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -69,7 +71,7 @@ export default function App() {
         <section className="all-movies">
           <h2>All Movies</h2>
           {isLoading ? (
-            <p className="text-white">Loading...</p>
+            <Spinner />
           ) : errorMessage ? (
             <p className="text-red-500">{errorMessage}</p>
           ) : (
